323c7bf4ede95b698bdc00a77ab7d3fae41bf809
rodrigoantoniocruz <rodrigoantoniocruz134@gmail.com>
2023-10-14 18:12:23 -0300

    Ajustes na estrutura

---
+++ app/spring/src/main/java/com/example/app/spring/ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.app.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example"})
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+}
